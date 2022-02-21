bfa7648d2bf0359a03efaee753e576f6643d6958
jsaleix <julian.saleix@gmail.com>
2022-02-21 21:52:43 +0100

    Pokedex started

--- src/Components/PkmItem.js
+++ src/Components/PkmItem.js
@@ -1,13 +1,24 @@
 import { Component, CreateElement } from '../../react/index.js';
 
-class PkmItem extends Component {
-  constructor(props){
-    super(props);
-  }
+const PKM_DEF_PICTURE = "https://naramsim.github.io/Colosseum/images/pokemons/";
 
-  toRender = () => {
-    return CreateElement('p', null, JSON.stringify(this?.props))
-  }
+class PkmItem extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    getImg = () => {
+        return `${PKM_DEF_PICTURE}${this.props.pkm.id}.svg`;
+    }
+
+    toRender = () => {
+        if(!this.props.pkm) return;
+        const img = CreateElement('img', {attributes:{src: this.getImg()}});
+        const name = CreateElement('h3', null, this?.props?.pkm?.name);
+
+        const div = CreateElement('div', {attributes: {class: 'pkm-item'}}, img, name);
+        return div;
+    }
 
 };
 
--- src/Pages/Pokedex.js
+++ src/Pages/Pokedex.js
@@ -12,14 +12,21 @@ class Pokedex extends Component {
                 .then( res => res.json())
                 .catch(e => console.error(e));
     if(res?.results){
-      this.setState({ pkms: res.results });
+      let tmpList = [];
+      res.results.forEach.call(res.results, pkm => {
+          let idPkm = (pkm.url).split("https://");
+          idPkm     = (idPkm[1]).split("/")[4];
+          let newPkm = {name: pkm.name, id: idPkm}
+          tmpList.push(newPkm);
+      });
+      this.setState({ pkms: tmpList });
     }
   };
 
   pkmItem = () => {
     if(this.state.pkms){
-      let pkms = this.state.pkms.map( p => CreateElement('p', {attributes: {class: 'pkm-item'}}, p.name));
-      let main = CreateElement('div', null, ...pkms);
+      let pkms = this.state.pkms.map( p => new PkmItem({pkm: p}) );
+      let main = CreateElement('div', { attributes: { class: 'pkm-content'}}, ...pkms);
       return main;
     }
   };
@@ -35,10 +42,9 @@ class Pokedex extends Component {
   toRender = () => {
     const home = CreateElement(
       'div', 
-      { attributes: {id: 'homepage'}},
+      { attributes: {id: 'pokedex'}},
       CreateElement('h1', {'onclick': this.updateTest}, 'Pokedex'),
-      this.pkmItem(),
-      new PkmItem({ test: this.state.test})
+      this.pkmItem()
     );
     return home;
   };
